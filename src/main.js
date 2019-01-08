829a1fe8afb8c19f3af711cbf2ca8bba164de253
MirellaLucero <lucero_200900@hotmail.com>
2019-01-08 18:33:44 -0500

    arreglando color de promedios

--- src/main.js
+++ src/main.js
@@ -119,3 +119,55 @@ selectOrder.addEventListener('change', () => {
   const ordered = pokemon.order(dataMainPokemon, valueSelect);
   paintPokemones(ordered);
 });
+
+
+let html, body, scrollToTopButton;
+window.onload = function() {
+  html = document.documentElement;
+  body = document.body;
+  scrollToTopButton = document.getElementById('scrollToTopButton');
+};
+
+function scrollToTop(totalTime, easingPower) {
+  // console.log("here");
+  const timeInterval = 1; // in ms
+  let scrollTop = Math.round(body.scrollTop || html.scrollTop);
+  // var by=- scrollTop;
+  let timeLeft = totalTime;
+  var scrollByPixel = setInterval(function() {
+    let percentSpent = (totalTime - timeLeft) / totalTime;
+    if (timeLeft >= 0) {
+      let newScrollTop = scrollTop * (1 - easeInOut(percentSpent, easingPower));
+      body.scrollTop = newScrollTop;
+      html.scrollTop = newScrollTop;
+      // console.log(easeInOut(percentSpent,easingPower));
+      timeLeft--;
+    } else {
+      clearInterval(scrollByPixel);
+      // Add hash to the url after scrolling
+      // window.location.hash = hash;
+    }
+  }, timeInterval);
+}
+
+function easeInOut(time, power) {
+  if (time < 0.5) {
+    return 0.5 * Math.pow(2 * time, power);
+  } else {
+    return 0.5 * (2 - Math.pow(2 * (1 - time), power));
+  }
+}
+
+window.onscroll = controlScrollToTopButton;
+
+function controlScrollToTopButton() {
+  let windowInnerHeight = 2 * window.innerHeight;
+  if (
+    body.scrollTop > windowInnerHeight ||
+    html.scrollTop > windowInnerHeight
+  ) {
+    scrollToTopButton.classList.add('show');
+  } else {
+    scrollToTopButton.classList.remove('show');
+  }
+}
